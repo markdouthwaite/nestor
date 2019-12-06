c6550164c5138b26e12014b0817eb8a8678b2a17
Mark Douthwaite <mark@douthwaite.io>
2019-12-06 21:28:40 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,9 +8,3 @@ alias kubectl=microk8s.kubectl
 
 microk8s.enable ingress registry
 microk8s.start
-
-#curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
-#chmod +x skaffold
-#mv skaffold /usr/local/bin
-#
-#skaffold build --default-repo localhost:32000
